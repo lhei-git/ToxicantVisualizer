0d40265acc20fdffafb3476862bf932ca591b2f6
Adwait Wadekar <adwaitwadekar97@gmail.com>
2020-09-28 17:18:00 -0400

    serve from csv

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from viewModule.views import demo
+from viewModule.views import demo, coord
 
 # TODO - establish URL patterns for calls (~python regular expressions)
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('demo/', demo),
     path('', demo),
     path('demo/<slug:tri_attr>', demo),
+    path('coord/<int:x1>/<int:y1>/<int:x2>/<int:y2>', coord),
 ]
