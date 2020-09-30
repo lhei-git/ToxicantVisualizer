ebf9f08d51b196e660aa0d9926d978d8a7e809d2
Adwait Wadekar <adwaitwadekar97@gmail.com>
2020-09-30 01:26:31 -0400

    complete coordinate API

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from viewModule.views import demo, coord
+from viewModule.views import demo, points, idview, chemview
 
 # TODO - establish URL patterns for calls (~python regular expressions)
 
@@ -9,5 +9,7 @@ urlpatterns = [
     path('demo/', demo),
     path('', demo),
     path('demo/<slug:tri_attr>', demo),
-    path('coord/<int:x1>/<int:y1>/<int:x2>/<int:y2>', coord),
+    path('points', points),
+    path('getid', idview),
+    path('getchem', chemview)
 ]
