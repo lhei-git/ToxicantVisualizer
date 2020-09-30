6f35b99acb7ccd6e7708b9e99c2f381ac19aca38
Adwait Wadekar <adwaitwadekar97@gmail.com>
2020-09-30 01:26:31 -0400

    complete coordinate API

--- backend/viewModule/views.py
+++ backend/viewModule/views.py
@@ -1,25 +1,36 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-import csv
 # This page handles requests by individual "view" functions
-# TODO - import django module to serve json
-# TODO - wire db/serve from csv
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from rest_framework.renderers import JSONRenderer
+from django.db.models import Q
+from viewModule.models import Tri2018 as tri
+from viewModule.serializers import Tri2018Serializer as t_szr
+from django.core import serializers as szs
 
-# (lat, long) x1, y1 -> NE and x2, y2 -> SW corner points
-def coord(request, x1=int(0), y1=int(0), x2=int(0), y2=int(0)):
-    csv_f = open('TRI_2018_US.csv', 'r')
-    reader_data = csv.reader(csv_f)
-    data_list = []
-    for line in reader_data:
-        data_list.append(line)
-    lat_out = []
-    long_out = []
-    for x in range(1, 101):
-        if(data_list[x][11] > x1 and data_list[x][12] < y1 and data_list[x][11] < x2 and data_list[x][12] > y2):
-            lat_out.append(data_list[x][11])
-            long_out.append(data_list[x][12])
+# NOTE - use limiters [:5] (first 5) to truncate result
+# view to get entry based on id
+def idview(request):
+    p_id = int(request.GET.get('id'))
+    result = tri.objects.get(id=p_id)
+    serializer = t_szr(result)
+    return JsonResponse(serializer.data) # no need to set safe to false since dict is returned (1 entry)
 
-    return HttpResponse('<h2> {} </h2>'.format(str(lat_out)))
+def chemview(request):
+    p_chem = str(request.GET.get('chemical'))
+    resultset = tri.objects.filter(chemical=p_chem)[:10]
+    data = szs.serialize('json', resultset) # django core sz for queryset
+    return JsonResponse(data, safe=False)
+
+def points(request):
+    # UC ex: tri.objects.filter(
+    # Q(latitude__lt=30.7)&Q(latitude__gt=30.6)&Q(longitude__lt=-96.3)&Q(longitude__gt=-96.5))
+    ne_lat = float(request.GET.get('ne_lat', default=0.0))
+    ne_lng = float(request.GET.get('ne_lng', default=0.0))
+    sw_lat = float(request.GET.get('sw_lat', default=0.0))
+    sw_lng = float(request.GET.get('sw_lng', default=0.0))
+    data = szs.serialize('json',tri.objects.filter(Q(latitude__lt=ne_lat)&Q(latitude__gt=sw_lat)
+                                                   &Q(longitude__lt=ne_lng)&Q(longitude__gt=sw_lng)))
+    return JsonResponse(data, safe=False)
 
 def demo(request, tri_attr=int(-9999)):
     if tri_attr == -9999:
@@ -28,6 +39,6 @@ def demo(request, tri_attr=int(-9999)):
         return HttpResponse('<h1>TRI data for attribute # {}</h1>'.format(tri_attr))
 
 # if request.method== 'GET': #filter request types my method attr
-# return HttpResponseNotFound(...) #returns 404
 # by using annot. @require_http_methods improper request returns 405 (not allowed)
-# @cache_page(900) for readability and performance - per view cache
+# more on complex queries via Q objects - Field lookups
+# - https://docs.djangoproject.com/en/3.1/ref/models/querysets/#field-lookups
