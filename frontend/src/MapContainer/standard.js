15016dcee8ecf375cd6515ec7bf6ee11b15b5cee
Evan de Jesus <evan.dejesus@24g.com>
2020-10-05 08:16:38 -0400

    add standard map style to remove unneeded icons

---
+++ frontend/src/MapContainer/standard.js
@@ -0,0 +1,13 @@
+/* This is where the style of the map is customized */
+module.exports = [
+  {
+    featureType: "poi",
+    elementType: "all",
+    stylers: [{ visibility: "off" }],
+  },
+  {
+    featureType: "transit",
+    elementType: "all",
+    stylers: [{ visibility: "off" }],
+  },
+];
