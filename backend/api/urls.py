61ef71f6ada520b881a6ea24e0d6e5727c5f1c2a
Adwait Wadekar <adwaitwadekar97@gmail.com>
2020-10-09 15:37:52 -0400

    release count endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from viewModule.views import demo, points, idview, chemview
-
-# TODO - establish URL patterns for calls (~python regular expressions)
+from viewModule.views import demo, points, idview, chemview, p_count
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +8,7 @@ urlpatterns = [
     path('', demo),
     path('demo/<slug:tri_attr>', demo),
     path('points', points),
+    path('trends/count', p_count),
     path('getid', idview),
     path('getchem', chemview)
 ]
