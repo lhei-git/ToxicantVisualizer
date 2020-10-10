93bc3c9570c36eea934708ef17ea50cddeea23aa
Adwait Wadekar <adwaitwadekar97@gmail.com>
2020-10-10 14:17:12 -0400

    search by attributes fr

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from viewModule.views import demo, points, idview, chemview, p_count
+from viewModule.views import demo, points, p_count, attr
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('demo/', demo),
     path('', demo),
     path('demo/<slug:tri_attr>', demo),
+    path('attr/<slug:attribute>', attr),
     path('points', points),
     path('trends/count', p_count),
-    path('getid', idview),
-    path('getchem', chemview)
 ]
