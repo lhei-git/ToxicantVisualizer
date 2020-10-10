a88a4738aa1eb436a603dbc824e5c9e0c0e534de
Adwait Wadekar <adwaitwadekar97@gmail.com>
2020-10-10 14:17:12 -0400

    search by attributes fr

--- backend/viewModule/views.py
+++ backend/viewModule/views.py
@@ -1,21 +1,46 @@
 # This page handles requests by individual "view" functions
 
 from django.http import HttpResponse, JsonResponse
+# from django.shortcuts import render # return render(request, template, context)
 from django.db.models import Q
 from viewModule.models import Tri2018 as tri
 from viewModule.serializers import Tri2018Serializer as t_szr
 from django.core import serializers as szs
 
+
+def attr(request, attribute=str()):
+    attr = str(attribute).upper()
+    if attr == 'ID':
+        return idview(request)
+    elif attr == 'CHEMICAL' or attr == 'CHEMICALS':
+        return chemview(request)
+    elif attr == 'CITY':
+        return cityview(request)
+    elif attr == 'ZIP':
+        return zipview(request)
+
 def idview(request):
     p_id = int(request.GET.get('id'))
     result = tri.objects.get(id=p_id)
     serializer = t_szr(result)
-    return JsonResponse(serializer.data) # no need to set safe to false since dict is returned (1 entry)
+    return JsonResponse(serializer.data)  # no need to set safe to false since dict is returned (1 entry)
 
 def chemview(request):
     p_chem = str(request.GET.get('chemical')).upper()
     resultset = tri.objects.filter(chemical=p_chem)[:10]
-    data = szs.serialize('json', resultset) # django core sz for queryset
+    data = szs.serialize('json', resultset)
+    return JsonResponse(data, safe=False)
+
+def cityview(request):
+    p_city = str(request.GET.get('city')).upper()
+    resultset = tri.objects.filter(city=p_city)[:10]
+    data = szs.serialize('json', resultset)
+    return JsonResponse(data, safe=False)
+
+def zipview(request):
+    p_zip = int(request.GET.get('zip'))
+    resultset = tri.objects.filter(zip=p_zip)
+    data = szs.serialize('json', resultset)
     return JsonResponse(data, safe=False)
 
 # SAMPLE -> /points?ne_lat=13.3950&sw_lat=13.3948&sw_lng=144.7070&ne_lng=144.7072 ==> 6 results in GUAM (2018)
@@ -24,8 +49,8 @@ def points(request):
     ne_lng = float(request.GET.get('ne_lng', default=0.0))
     sw_lat = float(request.GET.get('sw_lat', default=0.0))
     sw_lng = float(request.GET.get('sw_lng', default=0.0))
-    data = szs.serialize('json',tri.objects.filter(Q(latitude__lt=ne_lat)&Q(latitude__gt=sw_lat)
-                                                   &Q(longitude__lt=ne_lng)&Q(longitude__gt=sw_lng)))
+    data = szs.serialize('json', tri.objects.filter(Q(latitude__lt=ne_lat) & Q(latitude__gt=sw_lat)
+                                                    & Q(longitude__lt=ne_lng) & Q(longitude__gt=sw_lng)))
     return HttpResponse(data, content_type='application/json')
 
 def p_count(request):
@@ -36,16 +61,14 @@ def p_count(request):
     start = int(request.GET.get('start', default=2018))
     end = int(request.GET.get('end', default=2018))
     count = tri.objects.filter(Q(latitude__lt=ne_lat) & Q(latitude__gt=sw_lat)
-                                                    & Q(longitude__lt=ne_lng) & Q(longitude__gt=sw_lng)
-                                                    & Q(year__lte=end) & Q(year__gte=start)).count()
+                               & Q(longitude__lt=ne_lng) & Q(longitude__gt=sw_lng)
+                               & Q(year__lte=end) & Q(year__gte=start)).count()
     return HttpResponse(int(count), content_type='application/json')
-
 
 def demo(request, tri_attr=int(-9999)):
     if tri_attr == -9999:
         return HttpResponse('<h1>No attribute requested</h1>')
     else:
         return HttpResponse('<h1>TRI data for attribute # {}</h1>'.format(tri_attr))
-
 
 # - https://docs.djangoproject.com/en/3.1/ref/models/querysets/#field-lookups
