d12d84716b6dc569d794e51f68580f89236297e1
Adwait Wadekar <adwaitwadekar97@gmail.com>
2020-10-11 16:04:44 -0400

    rewired endpoints and edited db schema

--- backend/viewModule/models.py
+++ backend/viewModule/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
 
-class Tri2018(models.Model):
-
-    year = models.IntegerField(db_column='YEAR', blank=True, null=True)  
+class Tri(models.Model):
+    year = models.IntegerField(db_column='YEAR', blank=True, null=True)
     facilityname = models.TextField(db_column='FACILITYNAME', blank=True, null=True)
     streetaddress = models.TextField(db_column='STREETADDRESS', blank=True, null=True)
     city = models.TextField(db_column='CITY', blank=True, null=True)
@@ -12,15 +11,24 @@ class Tri2018(models.Model):
     zip = models.IntegerField(db_column='ZIP', blank=True, null=True)
     latitude = models.FloatField(db_column='LATITUDE', blank=True, null=True)
     longitude = models.FloatField(db_column='LONGITUDE', blank=True, null=True)
-    parentco = models.TextField(db_column='PARENTCO', blank=True, null=True)
+    parentconame = models.TextField(db_column='PARENTCONAME', blank=True, null=True)
     industrysector = models.TextField(db_column='INDUSTRYSECTOR', blank=True, null=True)
     chemical = models.TextField(db_column='CHEMICAL', blank=True, null=True)
-    cleanairact = models.TextField(db_column='CLEANAIRACT', blank=True, null=True)
-    id = models.AutoField(primary_key=True, blank=True)
-
-    def __str__(self):
-        return('\nPoisoning: {} \nFacility: {} \nCompany {}'.format(self.chemical, self.facilityname, self.parentco))
+    cleanairactchemical = models.TextField(db_column='CLEANAIRACTCHEMICAL', blank=True, null=True)
+    classification = models.TextField(db_column='CLASSIFICATION', blank=True, null=True)
+    carcinogen = models.TextField(db_column='CARCINOGEN', blank=True, null=True)
+    unitofmeasure = models.TextField(db_column='UNITOFMEASURE', blank=True, null=True)
+    fugitiveair = models.IntegerField(db_column='FUGITIVEAIR', blank=True, null=True)
+    stackair = models.IntegerField(db_column='STACKAIR', blank=True, null=True)
+    totalreleaseair = models.IntegerField(db_column='TOTALRELEASEAIR', blank=True, null=True)
+    totalreleasewater = models.IntegerField(db_column='TOTALRELEASEWATER', blank=True, null=True)
+    totalreleaseland = models.IntegerField(db_column='TOTALRELEASELAND', blank=True, null=True)
+    on_sitereleasetotal = models.IntegerField(db_column='ON-SITERELEASETOTAL', blank=True, null=True)
+    off_sitereleasetotal = models.IntegerField(db_column='OFF-SITERELEASETOTAL', blank=True, null=True)
+    totalreleases = models.IntegerField(db_column='TOTALRELEASES', blank=True, null=True)
+    one_timerelease = models.TextField(db_column='ONE-TIMERELEASE', blank=True, null=True)
+    id = models.AutoField(primary_key=True, db_column='ID')
 
     class Meta:
-        db_table = 'TRI_2018'
-        # managed = false # makes sure create and update signals don't affect db
+        # managed = False
+        db_table = 'TRI'
