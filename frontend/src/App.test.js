9c3724882b9776b3b1c0eef002ce50b3f1286553
Evan de Jesus <evan.dejesus@24g.com>
2020-10-21 09:14:26 -0400

    add initial render test

---
+++ frontend/src/App.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import App from "./App";
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("Renders site correctly", () => {
+  act(() => {
+    const setRouteLeaveHook = jest.fn();
+    render(<App.WrappedComponent params={{ router: setRouteLeaveHook }} />, container);
+  });
+  expect(container).toMatchSnapshot();
+});
