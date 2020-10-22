7508d241aeed7876a42f6ffeb392775010a255a0
Evan de Jesus <evan.dejesus@24g.com>
2020-10-22 10:40:15 -0400

    add pubchem data to sidebar with navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ import "./App.css";
 import "./index.css";
 import UserControlPanel from "./UserControlPanel";
 import { useReducer } from "react";
-import { getChemical } from "./helpers";
+import { formatChemical } from "./helpers";
 const React = require("react");
 
 const initialState = {
@@ -25,6 +25,8 @@ const initialState = {
   refreshed: false,
   lastSearch: "",
   chemicals: [],
+  showPubchemInfo: false,
+  currentChemical: "",
   filters: {
     dioxins: false,
     carcinogens: false,
@@ -41,20 +43,29 @@ const reducer = (state, action) => {
       return { ...state, numFacilities: action.payload };
     case "setFilters":
       return { ...state, filters: action.payload };
+    case "setCurrentChemical":
+      return { ...state, currentChemical: action.payload };
     case "setChemicals":
       return {
         ...state,
         chemicals: action.payload.map((c) => ({
           ...c,
-          name: getChemical(c.name),
+          name: formatChemical(c.name),
         })),
       };
     case "setLastSearch":
       localStorage.setItem("searchedLocation", state.location);
       return { ...state, lastSearch: action.payload };
+    case "showPubchemInfo":
+      return { ...state, showPubchemInfo: !state.showPubchemInfo };
     case "refresh":
       const old = state.refreshed;
-      return { ...state, refreshed: !old, chemicals: [] };
+      return {
+        ...state,
+        refreshed: !old,
+        chemicals: [],
+        showPubchemInfo: false,
+      };
     default:
       throw new Error();
   }
@@ -65,35 +76,58 @@ const setNumFacilities = (num) => ({ type: "setNumFacilities", payload: num });
 const setFilters = (filters) => ({ type: "setFilters", payload: filters });
 const refresh = () => ({ type: "refresh" });
 const setLastSearch = (search) => ({ type: "setLastSearch", payload: search });
+const showPubchemInfo = () => ({ type: "showPubchemInfo" });
+const setCurrentChemical = (chemical) => ({
+  type: "setCurrentChemical",
+  payload: chemical,
+});
 const setChemicals = (chemicals) => ({
   type: "setChemicals",
   payload: chemicals,
 });
+
+function ChemicalList(props) {
+  const { chemicals } = props;
+  if (chemicals.length === 0) return <div></div>;
+
+  const listItems = chemicals
+    .sort((a, b) => b.totalreleases - a.totalreleases)
+    .map((c) => (
+      <li
+        onClick={() => {
+          props.onClick(c.name);
+        }}
+        key={c.name + " " + c.totalreleases}
+      >
+        {c.name} ({c.totalreleases} lbs)
+      </li>
+    ));
+  return (
+    <div>
+      <ol>{listItems}</ol>
+    </div>
+  );
+}
+
+const Footer = () => {
+  const React = require("react");
+  return (
+    <div className="footer">
+      &#169;{" "}
+      <span>
+        VET was developed in 2018 for the Lab for Health and Environmental
+        Information
+      </span>
+    </div>
+  );
+};
 
 const App = (props) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   function handleSearchSubmit() {
     dispatch(setLastSearch(state.location));
-    // localStorage.setItem("searchedLocation", state.location);
     history.push("/map");
-  }
-
-  function renderChemicals(chemicals) {
-    if (chemicals.length === 0) return <div></div>;
-
-    const listItems = chemicals
-      .sort((a, b) => b.totalreleases - a.totalreleases)
-      .map((c) => (
-        <li key={c.name + " " + c.totalreleases}>
-          {c.name} ({c.totalreleases} lbs)
-        </li>
-      ));
-    return (
-      <div>
-        <ol>{listItems}</ol>
-      </div>
-    );
   }
 
   return (
@@ -135,9 +169,34 @@ const App = (props) => {
                     onRefresh={() => dispatch(refresh())}
                   ></UserControlPanel>
                 </div>
-                <div className="chemicals">
-                  {renderChemicals(state.chemicals)}
-                </div>
+                {state.showPubchemInfo ? (
+                  <div className="pubchem">
+                    <div
+                      className="back"
+                      onClick={() => {
+                        dispatch(showPubchemInfo());
+                      }}
+                    >
+                      &lt; Back to Chemicals
+                    </div>
+                    <PubChemFields chemName={state.currentChemical} />
+                  </div>
+                ) : (
+                  <div className="chemicals">
+                    {state.chemicals.length === 0 ? (
+                      <div></div>
+                    ) : (
+                      <div>Released Toxicants (click for info)</div>
+                    )}
+                    <ChemicalList
+                      onClick={(chemical) => {
+                        dispatch(showPubchemInfo());
+                        dispatch(setCurrentChemical(chemical));
+                      }}
+                      chemicals={state.chemicals}
+                    ></ChemicalList>
+                  </div>
+                )}
               </div>
 
               <div className="map-wrapper">
@@ -163,6 +222,7 @@ const App = (props) => {
             />
           </Route>
         </Switch>
+        <Footer />
       </div>
     </Router>
   );
--- frontend/src/helpers.js
+++ frontend/src/helpers.js
@@ -1,8 +1,25 @@
 // cleans up string format and removes unsearchable words
-module.exports.getChemical = (entry) => {
+module.exports.formatChemical = (entry) => {
   let trimmed = entry.toLowerCase().replace(/ *\([^)]*\) */g, "");
   const i = trimmed.search(/\band|compounds\b/);
   if (i !== -1) trimmed = trimmed.slice(0, i);
-  trimmed = trimmed.replace(/\b\w/g, (l) => l.toUpperCase());
+  trimmed = trimmed.replace(/\b\w/g, (l) => l.toUpperCase()).replace(/"/gi, "");
   return trimmed;
+};
+
+module.exports.shallowEqual = (obj1, obj2) => {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let key of keys1) {
+    if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+
+  return true;
 };
