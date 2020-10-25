22b74127f947f3a434ada9991f02cefeb44b2543
Adwait Wadekar <adwaitwadekar97@gmail.com>
2020-10-25 11:09:39 -0400

    post migration updates

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yb#re#b*nmjvxnxmno9qk-2owmi&b)77&8xm+=izl4v*w%i8^r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vet-pg-development.cjt2ltp5zdka.us-east-1.rds.amazonaws.com', 'localhost']
 
 
 # Application definition
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'viewModule',
-
 ]
 
 MIDDLEWARE = [
@@ -83,8 +82,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Y9fSIoOWwH1dHSCIl4Bi',
+        'HOST': 'vet-pg-development.cjt2ltp5zdka.us-east-1.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
@@ -113,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from viewModule.views import dist_fac, attr, num_facilities, state_total_releases, top_parentco_releases, chem_counts, location_summary, top_facility_releases
+from viewModule.views import points, attr, num_facilities, state_total_releases, top_parentco_releases, chem_counts, location_summary, top_facility_releases
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('attr/<slug:attribute>', attr),
     # return distinct facilities within: window, state
-    path('facilities', dist_fac),
+    path('facilities', points),
     # return number of facilities within: window
     path('stats/location/num_facilities', num_facilities),
     # return summary within: window
