759fb124d64795300c2bfb3846879740e02d8b81
Evan de Jesus <evan.dejesus@24g.com>
2020-11-26 16:47:40 -0500

    add api key authentication

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    'api.middleware.auth.AuthMiddleware'
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
