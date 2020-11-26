4e4864bd26aac9af5a030fdf3ea5b2c84be6cb96
adwait <adwaitwadekar97@gmail.com>
2020-11-25 21:16:43 -0500

    added geoFilter to all_facility_releases

--- backend/viewModule/views.py
+++ backend/viewModule/views.py
@@ -263,7 +263,6 @@ def all_chemicals_releases(request):
 
 
 ''' Returns all chemicals and respective total release (not by type / only total) amounts in queried location {Graph 15} '''
-
 def all_chemicals_total_releases(request):
     y = int(request.GET.get('year', default=latest_year))
     qs = release.objects.filter(geoFilter(request) & Q(year=y)).values('chemical__name').annotate(Sum('total')).order_by('-total')
@@ -273,14 +272,8 @@ def all_chemicals_total_releases(request):
 
 ''' Returns all facilities and respective total release (not by type / only total) amounts in queried location {Graph 12} '''
 def all_facility_releases(request):
-    ne_lat = float(request.GET.get('ne_lat', default=0.0))
-    ne_lng = float(request.GET.get('ne_lng', default=0.0))
-    sw_lat = float(request.GET.get('sw_lat', default=0.0))
-    sw_lng = float(request.GET.get('sw_lng', default=0.0))
     y = int(request.GET.get('year', default=latest_year))
-    window = Q(facility__latitude__lt=ne_lat) & Q(facility__latitude__gt=sw_lat) & Q(
-        facility__longitude__lt=ne_lng) & Q(facility__longitude__gt=sw_lng)
-    qs = release.objects.filter(window & Q(year=y)).values(
+    qs = release.objects.filter(geoFilter(request) & Q(year=y)).values(
         'facility__name').annotate(
         Sum('air'), Sum('water'), Sum('land'), Sum('off_site'))
     # print(qs.query)
@@ -301,11 +294,7 @@ def all_facility_total_releases(request):
     return JsonResponse(list(qs), content_type='application/json', safe=False)
 
 
-'''
-Return top 10 companies in total releases by geo window & year
-'''
-
-
+''' Return top 10 companies in total releases by geo window & year'''
 def top_parentco_releases(request):
     ne_lat = float(request.GET.get('ne_lat', default=0.0))
     ne_lng = float(request.GET.get('ne_lng', default=0.0))
