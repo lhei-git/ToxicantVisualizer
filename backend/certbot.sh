80db13df3dde6e795e1321c2c435ee5209068556
Evan de Jesus <evan.dejesus@24g.com>
2020-11-29 19:29:28 -0500

    finish setting up https

--- backend/certbot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sudo wget -r --no-parent -A 'epel-release-*.rpm' https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/
-sudo rpm -Uvh dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-*.rpm
-sudo yum-config-manager --enable epel*
-sudo yum install -y certbot python-certbot-nginx
