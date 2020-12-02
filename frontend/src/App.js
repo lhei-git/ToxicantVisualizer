7f8567ba0933a8273d9d30a2989ae28c6fe3b442
richmooremi <rich.moore.mi@gmail.com>
2020-12-02 13:07:18 -0500

    incremental

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -397,6 +397,7 @@ const App = (props) => {
                   </div>
                   {state.map.stateShort !== "US" && (
                     <div>
+                    {alert(state.map.stateShort)}
                       <ThematicStateMap
                         year={state.filters.year}
                         type={state.filters.releaseType}
