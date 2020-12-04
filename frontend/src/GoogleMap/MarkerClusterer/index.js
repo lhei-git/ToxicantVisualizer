3fbde5da97945574481a00f3cef52b893c809530
ejdejesu <evanjdejesus@gmail.com>
2020-12-03 23:20:47 -0500

    remove unnecessary effect hook

--- frontend/src/GoogleMap/MarkerClusterer/index.js
+++ frontend/src/GoogleMap/MarkerClusterer/index.js
@@ -16,82 +16,77 @@ const evtNames = [
 function MarkerCluster(props) {
   const { map, google, markers, releaseType } = props;
 
-  // This hook works like ComponentWillMount
-  // The  hook isn't really needed, this whole thing worked without it,
-  // I added the hook so that I could implement a cleanup function
-  useEffect(() => {
-    const handleEvent = ({ event, marker, entry }) => {
-      if (props[event]) {
-        props[event]({
-          props: props,
-          marker: marker,
-          event: event,
-          entry: entry,
-        });
-      }
-    };
-
-    if (map && markers) {
-      let type;
-      switch (String(releaseType)) {
-        case "air":
-          type = "grey";
-          break;
-        case "water":
-          type = "green";
-          break;
-        case "land":
-          type = "brown";
-          break;
-        case "off_site":
-          type = "yellow";
-          break;
-        default:
-          type = "red";
-          break;
-      }
-
-      const mapMarkers = markers.map((marker) => {
-        const entry = new google.maps.Marker({
-          position: {
-            lat: marker.position.lat,
-            lng: marker.position.lng,
-          },
-          map: map,
-          meta: marker.meta,
-          name: marker.name,
-          icon: {
-            url: require(`./../../../src/assets/${type}_${marker.color}-6.png`),
-            scaledSize: new google.maps.Size(21, 21),
-          },
-        });
-
-        evtNames.forEach((e) => {
-          entry.addListener(e, () =>
-            handleEvent({
-              event: e,
-              marker: marker,
-              entry: entry,
-            })
-          );
-        });
-
-        return entry;
+  const handleEvent = ({ event, marker, entry }) => {
+    if (props[event]) {
+      props[event]({
+        props: props,
+        marker: marker,
+        event: event,
+        entry: entry,
       });
-
-      const clusterer = new MarkerClusterer(map, mapMarkers, {
-        minimumClusterSize: props.minimumClusterSize,
-        imagePath:
-          "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-      });
-
-      // Cleanup function. Note, this is only returned if we create the markers
-      return () => {
-        //console.log('Cleaning up markers')
-        clusterer.clearMarkers();
-      };
     }
-  }, [map, google, markers, props.minimumClusterSize, releaseType]);
+  };
+
+  if (map && markers) {
+    let type;
+    switch (String(releaseType)) {
+      case "air":
+        type = "grey";
+        break;
+      case "water":
+        type = "green";
+        break;
+      case "land":
+        type = "brown";
+        break;
+      case "off_site":
+        type = "yellow";
+        break;
+      default:
+        type = "red";
+        break;
+    }
+
+    const mapMarkers = markers.map((marker) => {
+      const entry = new google.maps.Marker({
+        position: {
+          lat: marker.position.lat,
+          lng: marker.position.lng,
+        },
+        map: map,
+        meta: marker.meta,
+        name: marker.name,
+        icon: {
+          url: require(`./../../../src/assets/${type}_${marker.color}-6.png`),
+          scaledSize: new google.maps.Size(21, 21),
+        },
+      });
+
+      evtNames.forEach((e) => {
+        entry.addListener(e, () =>
+          handleEvent({
+            event: e,
+            marker: marker,
+            entry: entry,
+          })
+        );
+      });
+
+      return entry;
+    });
+
+    const clusterer = new MarkerClusterer(map, mapMarkers, {
+      minimumClusterSize: props.minimumClusterSize,
+      imagePath:
+        "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+    });
+
+    // Cleanup function. Note, this is only returned if we create the markers
+    return () => {
+      //console.log('Cleaning up markers')
+      clusterer.clearMarkers();
+    };
+  }
 
   // Do we need to render anything??
   return null;
