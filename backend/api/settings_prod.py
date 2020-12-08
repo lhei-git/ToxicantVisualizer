d21bc0cb4c65590dbf5f8e5e67e53ff0926c6ddd
ejdejesu <evanjdejesus@gmail.com>
2020-12-08 11:49:40 -0500

    remove prod secret key from source

--- backend/api/settings_prod.py
+++ backend/api/settings_prod.py
@@ -1,13 +1,12 @@
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yb#re#b*nmjvxnxmno9qk-2owmi&b)77&8xm+=izl4v*w%i8^r'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["api.vet.lhei.org"]
+ALLOWED_HOSTS = [os.environ.get('API_URL')]
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
