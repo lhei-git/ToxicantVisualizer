cc14b9df75252dd455a3e6186504cd5d4d111688
adwait <adwaitwadekar97@gmail.com>
2020-12-08 23:50:48 -0500

    remove old functionality and add comments

--- backend/viewModule/admin.py
+++ backend/viewModule/admin.py
@@ -1,6 +1,7 @@
+# Register your models here.
 from django.contrib import admin
 from .models import Facility, Chemical, Release
 admin.site.register(Facility)
 admin.site.register(Chemical)
 admin.site.register(Release)
-# Register your models here.
+
--- backend/viewModule/apps.py
+++ backend/viewModule/apps.py
@@ -2,6 +2,5 @@
 # See https://docs.djangoproject.com/en/3.1/ref/applications/#application-configuration
 from django.apps import AppConfig
 
-
 class ViewmoduleConfig(AppConfig):
     name = 'viewModule'
