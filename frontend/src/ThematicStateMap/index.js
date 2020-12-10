ad827735e04fbe60f9a6781e4cafd992c9862847
Rich Moore <rich.moore.mi@gmail.com>
2020-12-10 17:08:23 -0500

    Removed dead code from thematicMapView

--- frontend/src/ThematicStateMap/index.js
+++ frontend/src/ThematicStateMap/index.js
@@ -41,25 +41,7 @@ class ThematicStateMap extends Component {
       (releaseType !== "all" ? releaseType.replace("_", " ") : "").slice(1)
     );
   }
-
-  //convert applied filters into parenthetical text
-  getParentheticalString(filters) {
-    var strings = [];
-
-    if (filters.chemical != "all") strings.push(filters.chemical);
-    if (filters.pbt === true) strings.push("PBTs");
-    if (filters.carcinogen === true) strings.push("Carcinogens");
-
-    if (strings.length === 0) return "";
-    if (strings.length === 1) return " (" + strings[0] + " Only)";
-    if (strings.length === 2)
-      return " (" + strings[0] + " & " + strings[1] + " Only)";
-    if (strings.length === 3)
-      return (
-        " (" + strings[0] + ", " + strings[1] + " & " + strings[2] + " Only)"
-      );
-  }
-
+  
   // refetches data if the year or release type filter changed
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.filters !== this.props.filters) {
