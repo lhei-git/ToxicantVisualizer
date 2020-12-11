11bf833e2b02e0f75684e1f35bdd96df1a8611e7
adwait <adwaitwadekar97@gmail.com>
2020-12-11 14:25:46 -0500

    add client responses for endpoint status code checks

--- backend/viewModule/test.py
+++ backend/viewModule/test.py
@@ -6,5 +6,27 @@ class EndpointTestCases(TestCase):
         pass
 
     def test_status_codes(self):
+        
         get_facilities = self.client.get('/facilities?state=')
+        get_chemicals_in_window = self.client.get('/chemicals?state=')
+        get_chemicals = self.client.get('/facilities/XYZ/chemicals?state=')
+        location_summary =  self.client.get('/stats/location/summary?state=')
+        country_summary = self.client.get('/stats/summary?state=')
+        top_chemicals = self.cleint.get('/stats/location/top_chemicals?state=')
+        top_facility_releases = self.cleint.get('/stats/location/facility_releases?state=')
+        top_parentco_releases = self.cleint.get('/stats/location/parent_releases?state=')
+        timeline_total = self.cleint.get('/stats/location/timeline/total?state=')
+        timeline_top_chemicals = self.cleint.get(
+                                            '/stats/location/timeline/top_chemicals?state=')
+        timeline_top_pbt_chemicals = self.cleint.get(
+                                        '/stats/location/timeline/top_pbt_chemicals?state=')
+        timeline_top_facility_releases = self.cleint.get(
+                                        '/stats/location/timeline/facility_releases?state=')
+        timeline_top_parentco_releases = self.cleint.get(
+                                        '/stats/location/timeline/parent_releases?state=')
+        state_total_releases = self.cleint.get('/stats/state/summary?state=')
+        all_state_total_releases = self.cleint.get('/stats/state/all?state=')
+        all_county_total_releases = self.cleint.get('/stats/county/all?state=')
+
+
         self.assertEqual(get_facilities.status_code, 200)
