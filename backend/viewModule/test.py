85bbc7b7f628ed6b806aea6581e3906085df5a47
adwait <adwaitwadekar97@gmail.com>
2020-12-11 14:30:18 -0500

    remove console print commands and fix responses

--- backend/viewModule/test.py
+++ backend/viewModule/test.py
@@ -12,21 +12,22 @@ class EndpointTestCases(TestCase):
         get_chemicals = self.client.get('/facilities/XYZ/chemicals?state=')
         location_summary =  self.client.get('/stats/location/summary?state=')
         country_summary = self.client.get('/stats/summary?state=')
-        top_chemicals = self.cleint.get('/stats/location/top_chemicals?state=')
-        top_facility_releases = self.cleint.get('/stats/location/facility_releases?state=')
-        top_parentco_releases = self.cleint.get('/stats/location/parent_releases?state=')
-        timeline_total = self.cleint.get('/stats/location/timeline/total?state=')
-        timeline_top_chemicals = self.cleint.get(
+        top_chemicals = self.client.get('/stats/location/top_chemicals?state=')
+        top_facility_releases = self.client.get('/stats/location/facility_releases?state=')
+        top_parentco_releases = self.client.get('/stats/location/parent_releases?state=')
+        timeline_total = self.client.get('/stats/location/timeline/total?state=')
+        timeline_top_chemicals = self.client.get(
                                             '/stats/location/timeline/top_chemicals?state=')
-        timeline_top_pbt_chemicals = self.cleint.get(
+        timeline_top_pbt_chemicals = self.client.get(
                                         '/stats/location/timeline/top_pbt_chemicals?state=')
-        timeline_top_facility_releases = self.cleint.get(
+        timeline_top_facility_releases = self.client.get(
                                         '/stats/location/timeline/facility_releases?state=')
-        timeline_top_parentco_releases = self.cleint.get(
+        timeline_top_parentco_releases = self.client.get(
                                         '/stats/location/timeline/parent_releases?state=')
-        state_total_releases = self.cleint.get('/stats/state/summary?state=')
-        all_state_total_releases = self.cleint.get('/stats/state/all?state=')
-        all_county_total_releases = self.cleint.get('/stats/county/all?state=')
+        state_total_releases = self.client.get('/stats/state/summary?state=')
+        all_state_total_releases = self.client.get('/stats/state/all?state=')
+        all_county_total_releases = self.client.get('/stats/county/all?state=')
 
 
         self.assertEqual(get_facilities.status_code, 200)
+        self.assertEqual(get_chemicals_in_window.status_code, 200)
