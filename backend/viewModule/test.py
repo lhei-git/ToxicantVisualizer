bd95c593ff753db8495e8b755f06ad838324db0e
adwait <adwaitwadekar97@gmail.com>
2020-12-11 14:40:53 -0500

    add status code assertions

--- backend/viewModule/test.py
+++ backend/viewModule/test.py
@@ -29,5 +29,25 @@ class EndpointTestCases(TestCase):
         all_county_total_releases = self.client.get('/stats/county/all?state=')
 
 
-        self.assertEqual(get_facilities.status_code, 200)
-        self.assertEqual(get_chemicals_in_window.status_code, 200)
+        self.assertEqual(get_facilities.status_code, 200, 'Error: get_facilities')
+        self.assertEqual(get_chemicals_in_window.status_code, 200,
+                                                 'Error: get_chemicals_in_window')
+        self.assertEqual(get_chemicals.status_code, 200, 'Error: get_chemicals')
+        self.assertEqual(location_summary.status_code, 200, 'Error: location_summary')
+        self.assertEqual(country_summary.status_code, 200, 'Error: country_summary')
+        self.assertEqual(top_chemicals.status_code, 200, 'Error: top_chemicals')
+        self.assertEqual(top_facility_releases.status_code, 200, 'Error: top_facility_releases')
+        self.assertEqual(top_parentco_releases.status_code, 200, 'Error: top_parentco_releases')
+        self.assertEqual(timeline_total.status_code, 200, 'Error: timeline_total')
+        self.assertEqual(timeline_top_chemicals.status_code, 200, 'Error: timeline_top_chemicals')
+        self.assertEqual(timeline_top_pbt_chemicals.status_code, 200, 
+                                                            'Error: timeline_top_pbt_chemicals')
+        self.assertEqual(timeline_top_facility_releases.status_code, 200, 
+                                                        'Error: timeline_top_facility_releases')
+        self.assertEqual(timeline_top_parentco_releases.status_code, 200, 
+                                                        'Error: timeline_top_parentco_releases')
+        self.assertEqual(state_total_releases.status_code, 200, 'Error: state_total_releases')
+        self.assertEqual(all_state_total_releases.status_code, 200, 
+                                                            'Error: all_state_total_releases')
+        self.assertEqual(all_county_total_releases.status_code, 200, 
+                                                        'Error: all_county_total_releases')
